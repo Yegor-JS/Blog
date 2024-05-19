894bba45d9aba0a1af0cbc7243baf9e17169a0de
Egor <egor.k.lysenko@gmail.com>
2024-05-19 15:43:10 -0300

    add comments to articles

--- routers/content/articles.js
+++ routers/content/articles.js
@@ -9,4 +9,52 @@ router.get("/articles/:id", async (req, res) => {
   res.send(displayArticle(article));
 });
 
+// POSTING A COMMENT
+// ADD VALIDATION THAT THERE'S A COMMENT TO ADD
+router.post("/articles/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const changes = await articlesRepo.getOneBy({ id });
+    const commentBody = req.body.comment;
+    const commentId = articlesRepo.randomId();
+    const commentDate = Date();
+
+    if (!changes.comments) {
+      changes.comments = {};
+    }
+    
+    let commentCount = 1;
+    commentCount += Object.keys(changes.comments).length;
+
+    changes.comments["comment" + commentCount] = {
+      commentBody,
+      commentId,
+      commentDate,
+    };
+
+    await articlesRepo.update(id, changes);
+  } catch (err) {
+    return res.send(err, "Something went wrong");
+  }
+
+  res.redirect(req.originalUrl);
+});
+
+// router.post(
+//   "/admin/articles/:id/edit",
+//   requireAuth,
+//   requireAdmin,
+//   async (req, res) => {
+//     const articleWithLines = req.body;
+//     const changes = removeLinesFromArticle(articleWithLines);
+//     const id = req.params.id;
+//     try {
+//       await articlesRepo.update(id, changes);
+//     } catch (err) {
+//       return res.send(err, "Could not find item");
+//     }
+//     res.redirect("/admin");
+//   }
+// );
+
 module.exports = router;
--- views/content/article.js
+++ views/content/article.js
@@ -1,4 +1,18 @@
 module.exports = (article) => {
+  const getComments = () => {
+    const comments = article.comments;
+    const displayCommentsHere = document.createElement("div");
+
+    comments.forEach((comment) => {
+      const commentBody = document.createElement("div");
+      commentBody.innerHTML = comment.commentBody;
+      commentBody.className = `comment-${comments.indexOf(comment) + 1}`;
+      displayCommentsHere.appendChild(commentBody);
+    });
+
+    return displayCommentsHere;
+  };
+
   return `
     <!DOCTYPE html>
     <html lang="en">
@@ -11,6 +25,18 @@ module.exports = (article) => {
     <div>
     ${article.body}
     </div>
+    <div>
+Comments (${"NUMBER OF COMMENTS, ADD VARIABLE LATER"}):
+</div>
+${getComments()}
+<br><br>
+<form method="POST">
+<label for="body">Leave a comment:</label>
+<textarea id="bodyInput" style="height:400px;width:600px;font-size:14pt;"
+    placeholder="An interesting and respectful comment" name="body"></textarea><br><br>
+<button>Post</button>
+</form>
+
     </body>
     </html>
     `;
