6ae7a982a0c8097eecb20b14b18022f86914596b
Egor <egor.k.lysenko@gmail.com>
2024-06-26 19:16:20 -0300

    fix an issue with comments

--- public/displayComments.js
+++ public/displayComments.js
@@ -1,14 +1,13 @@
 function getComments(article) {
-  console.log('hi')
-    const comments = article.comments;
-    const displayCommentsHere = document.createElement("div");
-  
-    comments.forEach((comment) => {
-      const commentBody = document.createElement("div");
-      commentBody.innerHTML = comment.commentBody;
-      commentBody.className = `comment-${comments.indexOf(comment) + 1}`;
-      displayCommentsHere.appendChild(commentBody);
-    });
-  
-    return displayCommentsHere;
-  };
+  const comments = Object.values(article.comments || {});
+  const displayCommentsHere = document.createElement("div");
+
+  comments.forEach((comment) => {
+    const commentBody = document.createElement("div");
+    commentBody.innerHTML = comment.commentBody;
+    commentBody.className = `comment-${comments.indexOf(comment) + 1}`;
+    displayCommentsHere.appendChild(commentBody);
+  });
+
+  return displayCommentsHere;
+}
