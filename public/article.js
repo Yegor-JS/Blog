3426d0aec957c06229fdbedb888f85b6a53d9304
Egor <egor.k.lysenko@gmail.com>
2024-06-26 21:34:05 -0300

    fix a bug with comments

--- public/article.js
+++ public/article.js
@@ -37,7 +37,7 @@ Comments (${"NUMBER OF COMMENTS, ADD VARIABLE LATER"}):
 <form method="POST">
 <label for="body">Leave a comment:</label>
 <textarea id="bodyInput" style="height:400px;width:600px;font-size:14pt;"
-    placeholder="An interesting and respectful comment" name="body"></textarea><br><br>
+    placeholder="An interesting and respectful comment" name="comment"></textarea><br><br>
 <button>Post</button>
 </form>
 <script src="http://127.0.0.1:3000/displayComments.js"></script>
